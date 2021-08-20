d2533cf844ad33d1cf48677a6414750826b740bf
panchami shenoy <panchamishenoy16@gmail.com>
2021-08-20 17:02:55 +0530

    check employer is present or absent

--- employee_wage/src/employee_wage/employee_wage.java
+++ employee_wage/src/employee_wage/employee_wage.java
@@ -5,6 +5,14 @@ public class employee_wage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("welcome to employee wage comparision program");
+	
+final int IS_FULL_TIME =1;
+double empCheck=Math.floor(Math.random()*10)%2;
+if (empCheck==IS_FULL_TIME)
+	System.out.println("Employee is present");
+else
+	System.out.println("Employee is absent"); 
+	
 	}
 
 }
