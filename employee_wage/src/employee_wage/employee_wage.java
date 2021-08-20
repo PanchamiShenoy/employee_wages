20a27609486450c9b9a7cbc9da4bb2fc49658dd2
panchami shenoy <panchamishenoy16@gmail.com>
2021-08-20 16:52:46 +0530

    intial welcome message added

---
+++ employee_wage/src/employee_wage/employee_wage.java
@@ -0,0 +1,10 @@
+package employee_wage;
+
+public class employee_wage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to employee wage comparision program");
+	}
+
+}
