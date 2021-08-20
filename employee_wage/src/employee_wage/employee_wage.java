db55bf302873c8f3b8e123e0122ac235fb708dcd
panchami shenoy <panchamishenoy16@gmail.com>
2021-08-20 17:56:21 +0530

    calculate employee part time wage

--- employee_wage/src/employee_wage/employee_wage.java
+++ employee_wage/src/employee_wage/employee_wage.java
@@ -6,14 +6,18 @@ public class employee_wage {
 		// TODO Auto-generated method stub
 System.out.println("welcome to employee wage comparision program");
 	
-final int IS_FULL_TIME =1;
+ int IS_FULL_TIME =1;
+int is_part_time=2;
 int rate_per_hr =20;
 int emp_hrs=0;
 int emp_wage=0;
-double empCheck=Math.floor(Math.random()*10)%2;
+double empCheck=Math.floor(Math.random()*10)%3;
 if (empCheck==IS_FULL_TIME)
+	emp_hrs=16;
+else if (empCheck==is_part_time)
 	emp_hrs=8;
-	emp_wage=emp_hrs*rate_per_hr ;
+
+emp_wage=emp_hrs*rate_per_hr ;
 			
 	System.out.println("Employee wage is "+emp_wage);
 	
