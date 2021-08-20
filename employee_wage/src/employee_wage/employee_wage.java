f0b967762dd5c2f7161e8b12e7e2fcfb9a27c668
panchami shenoy <panchamishenoy16@gmail.com>
2021-08-20 20:46:48 +0530

    calculate emp wage using switch case

--- employee_wage/src/employee_wage/employee_wage.java
+++ employee_wage/src/employee_wage/employee_wage.java
@@ -6,16 +6,17 @@ public class employee_wage {
 		// TODO Auto-generated method stub
 System.out.println("welcome to employee wage comparision program");
 	
- int IS_FULL_TIME =1;
-int is_part_time=2;
-int rate_per_hr =20;
+final int IS_FULL_TIME =1;
+final int is_part_time=2;
+ int rate_per_hr =20;
 int emp_hrs=0;
 int emp_wage=0;
-double empCheck=Math.floor(Math.random()*10)%3;
-if (empCheck==IS_FULL_TIME)
-	emp_hrs=16;
-else if (empCheck==is_part_time)
-	emp_hrs=8;
+int empCheck=(int) (Math.floor(Math.random()*10)%3);
+switch (empCheck) {
+case  IS_FULL_TIME: emp_hrs=16;break;
+case is_part_time: emp_hrs=8;break;
+default: emp_hrs=0;
+}
 
 emp_wage=emp_hrs*rate_per_hr ;
 			
